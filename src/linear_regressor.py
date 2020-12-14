9e9904c153354fb5df5087fff74081b14ba920a3
Anton Perez <anton.sungho.perez@gmail.com>
2020-12-14 17:11:30 +0000

    updated linear regression

--- src/linear_regressor.py
+++ src/linear_regressor.py
@@ -7,18 +7,22 @@ class LinearRegressor:
   def __init__(self, dataframe, dependent_variable):
     self.dataframe = dataframe
     self.dependent_variable = dependent_variable
-    self.independent_variable = self.dataframe.columns[0] if self.dataframe.columns[0] != self.dependent_variable else self.dataframe.columns[1]
+    self.independent_variables = [var for var in self.dataframe.columns if var != self.dependent_variable]
     self.coefficients = self.calculate_coefficients()
-
+    
   def calculate_coefficients(self):
     data = self.dataframe.data_dict
-    indep_coeff = data[self.independent_variable]
+    indep_coeff = [data[self.independent_variables[i]] for i in range(len(self.independent_variables))]
     dependent = Matrix([data[self.dependent_variable]]).transpose()
-    coeff_mat = Matrix([[1] for _ in range(len(indep_coeff))]).augment(Matrix([indep_coeff]).transpose())
+    coeff_mat = Matrix([[1] for _ in range(len(indep_coeff[0]))]).augment(Matrix(indep_coeff).transpose())
     psuedo = coeff_mat.transpose()@coeff_mat
     final_coeff = psuedo.inverse()@(coeff_mat.transpose()@dependent) 
+    dict_keys = ['constant'] + self.independent_variables
 
-    return final_coeff.transpose().elements[0]
+    return {dict_keys[i]:final_coeff.transpose().elements[0][i] for i in range(len(dict_keys))}
   
-  def predict(self, input):
-    return self.coefficients[0] + self.coefficients[1]*input[self.independent_variable]
+  def predict(self, input_dict):
+    sum = self.coefficients['constant']
+    for var in self.independent_variables:
+      sum += self.coefficients[var]*input_dict[var]
+    return sum
--- tests/test_linear_regressor.py
+++ tests/test_linear_regressor.py
@@ -12,10 +12,37 @@ df = DataFrame.from_array(
 )
 regressor = LinearRegressor(df, dependent_variable='progress')
 
+# print('Testing attribute coefficients...')
+# assert [round(i,5) for i in regressor.coefficients] == [0.01667, 0.15] 
+# print('PASSED')
+
+# print('Testing method predict...')
+# assert round(regressor.predict({'hours worked': 4}),5) == 0.61667
+# print('PASSED')
+
+#Assignment 40
+df = DataFrame.from_array(
+    [[0, 0, 0.1],
+     [1, 0, 0.2],
+     [0, 2, 0.5],
+     [4,5,0.6]],
+    columns = ['scoops of chocolate', 'scoops of vanilla', 'taste rating']
+)
+regressor = LinearRegressor(df, dependent_variable='taste rating')
+
 print('Testing attribute coefficients...')
-assert [round(i,5) for i in regressor.coefficients] == [0.01667, 0.15] 
+assert {key:round(regressor.coefficients[key], 8) for key in regressor.coefficients} == {
+    'constant': 0.19252336,
+    'scoops of chocolate': -0.05981308,
+    'scoops of vanilla': 0.13271028
+}, {key:round(regressor.coefficients[key], 7) for key in regressor.coefficients}
 print('PASSED')
+# these coefficients are rounded, you should only round 
+# in your assert statement
 
 print('Testing method predict...')
-assert round(regressor.predict({'hours worked': 4}),5) == 0.61667
+assert round(regressor.predict({
+    'scoops of chocolate': 2,
+    'scoops of vanilla': 3
+    }),8) == 0.47102804
 print('PASSED')
